7d1323f383c27ddbe61263501687da410dc8d0d9
Miko Stern <mikomgk@gmail.com>
2019-11-07 20:42:51 +0200

    origin/master

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
         production: {
             user: 'appuser',
             host: '172.105.92.200',
-            ref: 'master',
+            ref: 'origin/master',
             repo: 'git@github.com:mikomgk/light-server.git',
             path: '/var/www/light-server',
             'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
