e958b89fdf47c1915e31496b13c12bf7acacf3c1
Miko Stern <mikomgk@gmail.com>
2019-11-16 17:12:51 +0200

    ssh key path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
 
     deploy: {
         production: {
-            key: '~/.ssh/id_rsa.pub',
+            key: '~/.ssh/id_rsa',
             user: 'appuser',
             host: '172.105.92.200',
             ref: 'origin/master',
