1b1dfbda98020613e065c631646657f56ba92fb6
Miko Stern <mikomgk@gmail.com>
2019-11-16 17:53:02 +0200

    ssh key path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,6 +13,7 @@ module.exports = {
 
     deploy: {
         production: {
+            key: '/Users/Miko/.ssh/id_rsa',
             user: 'appuser',
             host: '172.105.92.200',
             ref: 'origin/master',
